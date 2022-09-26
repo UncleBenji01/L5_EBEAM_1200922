5ccc6abadf1e2871097024c16baee1075e2d0fad
Estudiante <Estudiante@T203-03-2022>
2022-09-26 21:39:25 +0200

    Agregar archivos de proyecto.

---
+++ L5_EBEAM_1200922/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+int numeroEntero;
+
+Console.WriteLine("Ejercicio 1");
+Console.WriteLine("Ingresar un Numero Entero");
+
+numeroEntero = Convert.ToInt32(Console.ReadLine());
+
+if (numeroEntero > 0)
+{
+    Console.WriteLine("El Numero Es Positivo");
+}
+else if (numeroEntero < 0)
+{
+    Console.WriteLine(" El Numero Es Negativo ");
+
+}
+
+else if (numeroEntero == 0)
+{
+    Console.WriteLine("Cero");
+}
+
+Console.WriteLine(numeroEntero);    
